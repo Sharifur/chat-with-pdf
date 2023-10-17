3be5a56445d96c28163a949bb7c7d312f931c120
sharifur rahman <sharifurrahmanair@sharifurs-MacBook-Air.local>
2023-10-17 23:46:41 +0600

    aws s3 file upload added

--- src/components/ui/FiileUpload.tsx
+++ src/components/ui/FiileUpload.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { uploadToS3 } from "@/lib/aws";
 import { Inbox } from "lucide-react";
 import React, {useCallback} from "react";
 import { useDropzone } from "react-dropzone";
@@ -11,7 +12,23 @@ const FileUpload = () => {
 
     const {getRootProps,getInputProps,isDragActive} = useDropzone({
         accept: {"application/pdf" : ['.pdf']},
-        onDrop: (acceptedFiles) => {
+        onDrop: async (acceptedFiles) => {
+
+            const file = acceptedFiles[0];
+
+            if(file.size > 10 * 1024 * 1024){
+                alert("upload file less than 10MB");
+                return;
+            }
+
+
+            try{
+                const uploadedFile = await uploadToS3(file);
+                console.log('data', uploadedFile);
+            }catch(error){
+
+            }
+
             console.log(acceptedFiles);
         }
     });
---
+++ src/lib/aws.ts
@@ -0,0 +1,51 @@
+import AWS from "aws-sdk";
+
+
+export async function  uploadToS3(file: File){
+    try{
+        AWS.config.update({
+            accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_API_KEY,
+            secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY
+        });
+
+
+        //todo:: write function to upload to aws
+        const aws = new AWS.S3({
+            params: {
+                Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET
+            },
+            region : process.env.NEXT_PUBLIC_AWS_S3_REGION
+        })
+
+        //todo:: create file name
+        const file_name = "uploads/" +  Date.now().toString() + file.name.replace(" ","-");
+        const params = {
+            Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET!,
+            Key: file_name,
+            Body: file
+        }
+        console.log(process.env.NEXT_PUBLIC_AWS_S3_BUCKET);
+        const upload = aws.putObject(params).on('httpUploadProgress', evt => {
+            console.log(`upload file progress`,parseInt( ((evt.loaded * 100 ) / evt.total).toString() ))
+        }).promise();
+
+       await upload.then((data) => {
+            console.log(`file upload complete ${file_name}`);
+       })
+
+       return Promise.resolve({
+        file_key : file_name,
+        file_name : file.name
+       })
+
+    }catch(error){
+        console.log('eerrr',error);
+        // throw new Error(error);
+    }
+}
+
+export const getAwsFileUrl= (file_key : String)  => {
+    const url = `https://${process.env.NEXT_PUBLIC_AWS_S3_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_S3_REGION}.amazonaws.com/${file_key}`;
+
+    return url;
+}
