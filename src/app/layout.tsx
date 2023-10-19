86e708ac4bdd0830132d977891a62cfeb196ca72
sharifur rahman <sharifurrahmanair@sharifurs-MacBook-Air.local>
2023-10-19 22:14:51 +0600

    react query added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google';
 import {ClerkProvider} from "@clerk/nextjs";
+import { QueryProvider } from '@/provider/QueryProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
+      <QueryProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
+      </QueryProvider>
   </ClerkProvider>
   )
 }
